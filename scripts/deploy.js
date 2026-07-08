0a7d5bea0645f136acb704b7c622ed96a8f8045d
baoday97 <161543862+Nguyen-Bao11@users.noreply.github.com>
2026-07-08 08:33:08 +0700

    Add deploy script for HelloOPN contract

---
+++ scripts/deploy.js
@@ -0,0 +1,21 @@
+const hre = require("hardhat");
+
+async function main() {
+
+    const HelloOPN = await hre.ethers.deployContract("HelloOPN");
+
+    await HelloOPN.waitForDeployment();
+
+    console.log("Contract deployed to:");
+
+    console.log(await HelloOPN.getAddress());
+
+}
+
+main().catch((error)=>{
+
+    console.error(error);
+
+    process.exitCode = 1;
+
+});
