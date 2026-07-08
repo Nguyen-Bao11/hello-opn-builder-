fb6550c3bb628b03cbc271c769bbe5ae2f2639e7
baoday97 <161543862+Nguyen-Bao11@users.noreply.github.com>
2026-07-08 08:32:13 +0700

    Add HelloOPN contract with message management

--- contracts/HelloOPN.sol
+++ contracts/HelloOPN.sol
@@ -1,1 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
 
+contract HelloOPN {
+
+    string public message;
+
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+        message = "Hello OPN Builder!";
+    }
+
+    function setMessage(string memory newMessage) public {
+
+        require(msg.sender == owner, "Only owner");
+
+        message = newMessage;
+    }
+
+    function getMessage() public view returns(string memory){
+
+        return message;
+
+    }
+}
